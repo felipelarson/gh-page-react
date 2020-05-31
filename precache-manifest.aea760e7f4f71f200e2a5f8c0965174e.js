656efee130328cac5a6f27ae2513be95378239d7
Dom_Silveira <felipelarson@msn.com>
2020-05-31 12:54:54 +0100

    Updates

---
+++ precache-manifest.aea760e7f4f71f200e2a5f8c0965174e.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "17c08f528087641fea3dd1a1d0a2f954",
+    "url": "/gh-page-react/index.html"
+  },
+  {
+    "revision": "10256b7e1b61f2787c03",
+    "url": "/gh-page-react/static/css/main.dcbecb9a.chunk.css"
+  },
+  {
+    "revision": "2347cdc24eb7b69b7300",
+    "url": "/gh-page-react/static/js/2.1757aeb3.chunk.js"
+  },
+  {
+    "revision": "bc645b8808bddaa3c0c8d72dff721b3b",
+    "url": "/gh-page-react/static/js/2.1757aeb3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "10256b7e1b61f2787c03",
+    "url": "/gh-page-react/static/js/main.0d6b990f.chunk.js"
+  },
+  {
+    "revision": "17136ff93bebc6a78e17",
+    "url": "/gh-page-react/static/js/runtime-main.0bdc23ef.js"
+  },
+  {
+    "revision": "f81628e306880532fd1d9856b8fc6a01",
+    "url": "/gh-page-react/static/media/1.f81628e3.jpg"
+  },
+  {
+    "revision": "a402ddce2c51b5f3d2862d86f8700f49",
+    "url": "/gh-page-react/static/media/2.a402ddce.jpg"
+  },
+  {
+    "revision": "8be2c0fe6165449ec41de16778a8b425",
+    "url": "/gh-page-react/static/media/3.8be2c0fe.jpg"
+  },
+  {
+    "revision": "e653297fdbbfb57236658cff59ed92d5",
+    "url": "/gh-page-react/static/media/4.e653297f.jpg"
+  },
+  {
+    "revision": "0ff11d87e9ad6fe638ce64763202260d",
+    "url": "/gh-page-react/static/media/5.0ff11d87.jpg"
+  }
+]);
