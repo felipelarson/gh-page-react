2181ac35706bf22e352b6b8992ff8e4fbcbd3bae
Dom_Silveira <felipelarson@msn.com>
2020-05-31 12:47:28 +0100

    initials version

---
+++ build/precache-manifest.27003edc3caac15b771dfd69e7a622d7.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "41a072c43fcc4e81f2cd49925f4179a0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "10256b7e1b61f2787c03",
+    "url": "/static/css/main.dcbecb9a.chunk.css"
+  },
+  {
+    "revision": "2347cdc24eb7b69b7300",
+    "url": "/static/js/2.1757aeb3.chunk.js"
+  },
+  {
+    "revision": "bc645b8808bddaa3c0c8d72dff721b3b",
+    "url": "/static/js/2.1757aeb3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "10256b7e1b61f2787c03",
+    "url": "/static/js/main.0d6b990f.chunk.js"
+  },
+  {
+    "revision": "501843e19f59a0e5f9ac",
+    "url": "/static/js/runtime-main.0b19ce9f.js"
+  },
+  {
+    "revision": "f81628e306880532fd1d9856b8fc6a01",
+    "url": "/static/media/1.f81628e3.jpg"
+  },
+  {
+    "revision": "a402ddce2c51b5f3d2862d86f8700f49",
+    "url": "/static/media/2.a402ddce.jpg"
+  },
+  {
+    "revision": "8be2c0fe6165449ec41de16778a8b425",
+    "url": "/static/media/3.8be2c0fe.jpg"
+  },
+  {
+    "revision": "e653297fdbbfb57236658cff59ed92d5",
+    "url": "/static/media/4.e653297f.jpg"
+  },
+  {
+    "revision": "0ff11d87e9ad6fe638ce64763202260d",
+    "url": "/static/media/5.0ff11d87.jpg"
+  }
+]);
